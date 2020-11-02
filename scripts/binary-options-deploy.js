c723ab556a3e8e26c920f88ffd848a03ee591be2
Fernando Caamano <guettomusick@gmail.com>
2020-11-02 13:43:12 -0300

    Add comments and move pricefeed to deployment script

--- scripts/binary-options-deploy.js
+++ scripts/binary-options-deploy.js
@@ -4,12 +4,17 @@ const fs = require('fs');
 const networksPath = './client/src/artifacts/contracts/BinaryOptions.sol/networks.json';
 const networks = fs.existsSync(networksPath) ? JSON.parse(fs.readFileSync(networksPath) || '{}') : {};
 
+const priceFeeds = {
+  eth: '0x9326BFA02ADD2366b30bacB125260Af641031331',
+  link: '0x396c5E36DD0a0F5a5D33dae44368D4193f69a1F0',
+};
+
 async function main() {
   await hre.run('compile');
 
   // We get the contract to deploy
   const BinaryOptions = await hre.ethers.getContractFactory('BinaryOptions');
-  const binaryOptions = await BinaryOptions.deploy();
+  const binaryOptions = await BinaryOptions.deploy(priceFeeds.eth, priceFeeds.link);
 
   await binaryOptions.deployed();
 
