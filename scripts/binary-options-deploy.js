e28ce40587fbd86a2a7af539a05744de4f14ea47
Fernando Caamano <guettomusick@gmail.com>
2020-11-02 14:33:51 -0300

    Add price feeds for other networks

--- scripts/binary-options-deploy.js
+++ scripts/binary-options-deploy.js
@@ -5,16 +5,33 @@ const networksPath = './client/src/artifacts/contracts/BinaryOptions.sol/network
 const networks = fs.existsSync(networksPath) ? JSON.parse(fs.readFileSync(networksPath) || '{}') : {};
 
 const priceFeeds = {
-  eth: '0x9326BFA02ADD2366b30bacB125260Af641031331',
-  link: '0x396c5E36DD0a0F5a5D33dae44368D4193f69a1F0',
+  1: {
+    eth: '0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419',
+    link: '0x2c1d072e956AFFC0D435Cb7AC38EF18d24d9127c',
+  },
+  4: {
+    eth: '0x8A753747A1Fa494EC906cE90E9f37563A8AF630e',
+    link: '0xd8bD0a1cB028a31AA859A21A3758685a95dE4623',
+  },
+  42: {
+    eth: '0x9326BFA02ADD2366b30bacB125260Af641031331',
+    link: '0x396c5E36DD0a0F5a5D33dae44368D4193f69a1F0',
+  },
+  default: {
+    // TODO: Invalid, just for deploying ok
+    eth: '0x9326BFA02ADD2366b30bacB125260Af641031331',
+    link: '0x396c5E36DD0a0F5a5D33dae44368D4193f69a1F0',
+  }
 };
 
 async function main() {
   await hre.run('compile');
 
+  const priceFeed = priceFeeds[hre.network.config.chainId] || priceFeeds.default;
+
   // We get the contract to deploy
   const BinaryOptions = await hre.ethers.getContractFactory('BinaryOptions');
-  const binaryOptions = await BinaryOptions.deploy(priceFeeds.eth, priceFeeds.link);
+  const binaryOptions = await BinaryOptions.deploy(priceFeed.eth, priceFeed.link);
 
   await binaryOptions.deployed();
 
