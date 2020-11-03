65539c771231fa93525537c43932b9ad7d210bda
Fernando Caamano <guettomusick@gmail.com>
2020-11-03 20:43:19 -0300

    Add more tests for place, execute and collect

--- test/BinaryOptions.ts
+++ test/BinaryOptions.ts
@@ -1,9 +1,11 @@
 import { waffle, ethers } from 'hardhat';
 import { Signer, Contract } from 'ethers';
-
 import { expect } from 'chai';
 
+import { timeTravel, timeSet, timeSave, timeRevert } from './utils/time';
+
 import AggregatorV3Interface from '../client/src/artifacts/contracts/AggregatorV3Interface.sol/AggregatorV3Interface.json';
+import { MockContract } from 'ethereum-waffle';
 
 const {
   deployMockContract,
@@ -15,28 +17,48 @@ const getEthers = (amount: number, price = DEFAULT_PRICE) => amount * price;
 
 const MINUTE = 60;
 const INTERVAL = 10 * MINUTE;
-const getRound = (index: number) => Math.floor(Date.now()/1000/INTERVAL)*INTERVAL + index*INTERVAL;
-const getOldRound = () => getRound(-1);
-const getLastRound = () => getRound(0);
-const getNextRound = () => getRound(1);
-const getNextPlayableRound = () => getRound(2);
 
 const ETH_PRICE = 300;
 const DEF_PAYOUT = 80000;
 
 describe('BinaryOptions', function() {
+  let EthPriceFeed: MockContract;
   let binaryOptions: Contract;
   let binToken: Contract;
   let accounts: Signer[];
   let owner: Signer;
   let sender: Signer;
+  let initialBlockTime: number;
+  let snapshotId: number;
+
+  const getRound = (index: number = 0) => initialBlockTime + index*INTERVAL;
+  const getOldRound = () => getRound(-1);
+  const getLastRound = () => getRound(0);
+  const getNextRound = () => getRound(1);
+  const getNextPlayableRound = () => getRound(2);
+
+  before(async () => {
+    // Set base initial time to the begining of a round to avoid unwanted round change during test
+    initialBlockTime = Math.floor(Date.now()/1000/INTERVAL)*INTERVAL + 2*INTERVAL;
+    await timeSet(initialBlockTime);
+  });
+
+  beforeEach(async () => {
+    // Take snapshot of blockchain
+    snapshotId = await timeSave();
+  });
+
+  afterEach(async () => {
+    // Revert snapshot and timing
+    await timeRevert(snapshotId);
+  });
 
   beforeEach(async () => {
     accounts = await ethers.getSigners();
     owner = accounts[0];
     sender = accounts[1];
 
-    const EthPriceFeed = await deployMockContract(owner, AggregatorV3Interface.abi);
+    EthPriceFeed = await deployMockContract(owner, AggregatorV3Interface.abi);
     await EthPriceFeed.mock.latestRoundData.returns(0, ETH_PRICE, getLastRound(), getLastRound(), 0);
 
     const BinaryOptions = await ethers.getContractFactory('BinaryOptions');
@@ -141,110 +163,212 @@ describe('BinaryOptions', function() {
     });
   });
 
-  describe('Place, Execute and Collect', () => {
-    describe('ExecuteRound', () => {
-      it('should revert if no owner try to execute round', async () => {
-        await expect(
-          binaryOptions.connect(sender).executeRound(0)
-        ).to.be.revertedWith('Only owner can execute round');
-      });
+  describe('ExecuteRound', () => {
+    it('should revert if no owner try to execute round', async () => {
+      await expect(
+        binaryOptions.connect(sender).executeRound(0)
+      ).to.be.revertedWith('Only owner can execute round');
+    });
 
-      it('should revert if trying to execute future round', async () => {
-        await expect(
-          binaryOptions.executeRound(getNextRound())
-        ).to.be.revertedWith('Can\'t execute a future round');
+    it('should revert if trying to execute future round', async () => {
+      await expect(
+        binaryOptions.executeRound(getNextRound())
+      ).to.be.revertedWith('Can\'t execute a future round');
+    });
+
+    it('should revert if interval is not multiple of interval', async () => {
+      await expect(
+        binaryOptions.executeRound(getLastRound()+1)
+      ).to.be.revertedWith('Timestamp must be multiple of interval');
+    });
+
+    it('should revert if no data on round', async () => {
+      await expect(
+        binaryOptions.executeRound(getLastRound())
+      ).to.be.revertedWith('No data for current round');
+    });
+  });
+
+  describe('Place', () => {
+    beforeEach(async () => {
+      await binaryOptions.connect(sender).buy({ value: 100 });
+    });
+
+    it('should revert if amount is 0', async () => {
+      await expect(
+        binaryOptions.connect(sender).place(getNextPlayableRound(), 0, true)
+      ).to.be.revertedWith('You need to send some BIN');
+    });
+
+    it('should revert if no enough allowance', async () => {
+      await expect(
+        binaryOptions.connect(sender).place(getNextPlayableRound(), getTokens(1), true)
+      ).to.be.revertedWith('Check the token allowance');
+    });
+
+    describe('with Allowance', () => {
+      beforeEach(async () => {
+        await binToken.connect(sender).approve(binaryOptions.address, ethers.constants.MaxUint256);
       });
 
       it('should revert if interval is not multiple of interval', async () => {
         await expect(
-          binaryOptions.executeRound(getLastRound()+1)
+          binaryOptions.connect(sender).place(getNextPlayableRound()+1, getTokens(1), true)
         ).to.be.revertedWith('Timestamp must be multiple of interval');
       });
 
-      it('should revert if no data on round', async () => {
+      it('should revert if betting for next or past round', async () => {
         await expect(
-          binaryOptions.executeRound(getLastRound())
-        ).to.be.revertedWith('No data for current round');
+          binaryOptions.connect(sender).place(getNextRound(), getTokens(1), true)
+        ).to.be.revertedWith('You can\'t bet for next round');
+
+        await expect(
+          binaryOptions.connect(sender).place(getLastRound(), getTokens(1), true)
+        ).to.be.revertedWith('You can\'t bet for next round');
+
+        await expect(
+          binaryOptions.connect(sender).place(getOldRound(), getTokens(1), true)
+        ).to.be.revertedWith('You can\'t bet for next round');
       });
 
-      // TODO
-      // it('should revert if already executed', async () => {
-      //   await expect(
-      //     binaryOptions.executeRound(getLastRound())
-      //   ).to.be.revertedWith('Round already executed');
-      // });
+      it('should place one option', async () => {
+        const timeStamp = getNextPlayableRound();
+        await binaryOptions.connect(sender).place(timeStamp, getTokens(1), true);
+        
+        // token transfer from wallet
+        expect(await binToken.balanceOf(await sender.getAddress())).to.equal(getTokens(99));
+        // tokens not burned
+        expect(await binToken.totalSupply()).to.equal(getTokens(100));
+        // tokens transferred to contract
+        expect(await binToken.balanceOf(binaryOptions.address)).to.equal(getTokens(1));
+        // option created
+        expect(await binaryOptions.getOptionsLength()).to.equal(1);
+
+        const round = await binaryOptions.rounds(timeStamp);
+        const pendingOption = await binaryOptions.pendingOptions(await sender.getAddress(), 0);
+        const options = await binaryOptions.options(0);
+
+        expect(round.higherAmount).to.equal(getTokens(1));
+        expect(pendingOption).to.equal(0);
+        expect(options.price).to.equal(ETH_PRICE);
+        expect(options.higher).to.equal(true);
+        expect(options.execute).to.equal(timeStamp);
+        expect(options.amount).to.equal(getTokens(1));
+        expect(options.payout).to.equal(DEF_PAYOUT);
+        expect(options.buyer).to.equal(await sender.getAddress());
+        expect(options.winner).to.equal(false);
+      });
+
+      it('should add to both higher and lower at round', async () => {
+        const timeStamp = getNextPlayableRound();
+        await binaryOptions.connect(sender).place(timeStamp, getTokens(1), true);
+        await binaryOptions.connect(sender).place(timeStamp, getTokens(2), false);
+
+        expect(await binaryOptions.getOptionsLength()).to.equal(2);
+        expect(await binaryOptions.getPendingOptionsLength(await sender.getAddress())).to.equal(2);
+        expect(await binaryOptions.getCollectedOptionsLength(await sender.getAddress())).to.equal(0);
+        expect(await binaryOptions.getOptionsAtRoundLength(timeStamp)).to.equal(2);
+
+        const round = await binaryOptions.rounds(timeStamp);
+
+        expect(round.higherAmount).to.equal(getTokens(1));
+        expect(round.lowerAmount).to.equal(getTokens(2));
+      });
+    });
+  });
+
+  describe('Place & Execute', () => {
+    let timeStamp: number;
+
+    beforeEach(async () => {
+      await binaryOptions.connect(sender).buy({ value: 1 });
+      await binToken.connect(sender).approve(binaryOptions.address, ethers.constants.MaxUint256);
+      timeStamp = getNextPlayableRound();
     });
 
-    describe('Place', () => {
-      beforeEach(async () => {
-        await binaryOptions.connect(sender).buy({ value: 100 });
-      });
+    it('should mark as executed, set price and revert if already executed', async () => {
+      await binaryOptions.connect(sender).place(timeStamp, getTokens(1), true);
+      timeTravel(INTERVAL*2);
+      await EthPriceFeed.mock.latestRoundData.returns(0, ETH_PRICE + 100, getLastRound(), getLastRound(), 0);
+      await binaryOptions.executeRound(timeStamp);
 
-      it('should revert if amount is 0', async () => {
-        await expect(
-          binaryOptions.connect(sender).place(getNextPlayableRound(), 0, true)
-        ).to.be.revertedWith('You need to send some BIN');
-      });
+      await expect(
+        binaryOptions.executeRound(timeStamp)
+      ).to.be.revertedWith('Round already executed');
 
-      it('should revert if no enough allowance', async () => {
-        await expect(
-          binaryOptions.connect(sender).place(getNextPlayableRound(), getTokens(1), true)
-        ).to.be.revertedWith('Check the token allowance');
-      });
+      const round = await binaryOptions.rounds(timeStamp);
+      expect(round.executed).to.equal(true);
+      expect(round.price).to.equal(ETH_PRICE + 100);
+    });
+  });
 
-      describe('with Allowance', () => {
-        beforeEach(async () => {
-          await binToken.connect(sender).approve(binaryOptions.address, ethers.constants.MaxUint256);
-        });
+  describe('Place, Execute & Collect', () => {
+    let timeStamp: number;
 
-        it('should revert if interval is not multiple of interval', async () => {
-          await expect(
-            binaryOptions.connect(sender).place(getNextPlayableRound()+1, getTokens(1), true)
-          ).to.be.revertedWith('Timestamp must be multiple of interval');
-        });
+    beforeEach(async () => {
+      await binaryOptions.connect(sender).buy({ value: 1 });
+      await binToken.connect(sender).approve(binaryOptions.address, ethers.constants.MaxUint256);
+      timeStamp = getNextPlayableRound();
+    });
 
-        it('should revert if betting for next or past round', async () => {
-          await expect(
-            binaryOptions.connect(sender).place(getNextRound(), getTokens(1), true)
-          ).to.be.revertedWith('You can\'t bet for next round');
+    it('should collect if winner bet for higher', async () => {
+      await binaryOptions.connect(sender).place(timeStamp, getTokens(1), true);
+      timeTravel(INTERVAL*2);
+      await EthPriceFeed.mock.latestRoundData.returns(0, ETH_PRICE + 100, getLastRound(), getLastRound(), 0);
+      await binaryOptions.executeRound(timeStamp);
+      await binaryOptions.connect(sender).collect(); 
 
-          await expect(
-            binaryOptions.connect(sender).place(getLastRound(), getTokens(1), true)
-          ).to.be.revertedWith('You can\'t bet for next round');
+      expect(await binaryOptions.getOptionsLength()).to.equal(1);
+      expect(await binaryOptions.getPendingOptionsLength(await sender.getAddress())).to.equal(0);
+      expect(await binaryOptions.getCollectedOptionsLength(await sender.getAddress())).to.equal(1);
+      
+      expect(await binToken.balanceOf(await sender.getAddress())).to.equal(getTokens(1*(1+DEF_PAYOUT/100000)));
+      expect(await binToken.balanceOf(binaryOptions.address)).to.equal(0);
+    });
 
-          await expect(
-            binaryOptions.connect(sender).place(getOldRound(), getTokens(1), true)
-          ).to.be.revertedWith('You can\'t bet for next round');
-        });
+    it('should not collect if looser bet for higher', async () => {
+      await binaryOptions.connect(sender).place(timeStamp, getTokens(1), true);
+      timeTravel(INTERVAL*2);
+      await EthPriceFeed.mock.latestRoundData.returns(0, ETH_PRICE - 100, getLastRound(), getLastRound(), 0);
+      await binaryOptions.executeRound(timeStamp);
+      await binaryOptions.connect(sender).collect();
 
-        it('should place one option', async () => {
-          const timeStamp = getNextPlayableRound();
-          await binaryOptions.connect(sender).place(timeStamp, getTokens(1), true);
-          
-          // token transfer from wallet
-          expect(await binToken.balanceOf(await sender.getAddress())).to.equal(getTokens(99));
-          // tokens not burned
-          expect(await binToken.totalSupply()).to.equal(getTokens(100));
-          // tokens transferred to contract
-          expect(await binToken.balanceOf(binaryOptions.address)).to.equal(getTokens(1));
-          // option created
-          expect(await binaryOptions.getOptionsLength()).to.equal(1);
+      expect(await binaryOptions.getOptionsLength()).to.equal(1);
+      expect(await binaryOptions.getPendingOptionsLength(await sender.getAddress())).to.equal(0);
+      expect(await binaryOptions.getCollectedOptionsLength(await sender.getAddress())).to.equal(1);
+      
+      expect(await binToken.balanceOf(await sender.getAddress())).to.equal(0);
+      expect(await binToken.balanceOf(binaryOptions.address)).to.equal(getTokens(1));
+    });
 
-          const round = await binaryOptions.rounds(timeStamp);
-          const pendingOption = await binaryOptions.pendingOptions(await sender.getAddress(), 0);
-          const options = await binaryOptions.options(0);
+    it('should collect if winner bet for lower', async () => {
+      await binaryOptions.connect(sender).place(timeStamp, getTokens(1), false);
+      timeTravel(INTERVAL*2);
+      await EthPriceFeed.mock.latestRoundData.returns(0, ETH_PRICE - 100, getLastRound(), getLastRound(), 0);
+      await binaryOptions.executeRound(timeStamp);
+      await binaryOptions.connect(sender).collect(); 
 
-          expect(round.higherAmount).to.equal(getTokens(1));
-          expect(pendingOption).to.equal(0);
-          expect(options.price).to.equal(ETH_PRICE);
-          expect(options.higher).to.equal(true);
-          expect(options.execute).to.equal(timeStamp);
-          expect(options.amount).to.equal(getTokens(1));
-          expect(options.payout).to.equal(DEF_PAYOUT);
-          expect(options.buyer).to.equal(await sender.getAddress());
-          expect(options.winner).to.equal(false);
-        });
-      });
+      expect(await binaryOptions.getOptionsLength()).to.equal(1);
+      expect(await binaryOptions.getPendingOptionsLength(await sender.getAddress())).to.equal(0);
+      expect(await binaryOptions.getCollectedOptionsLength(await sender.getAddress())).to.equal(1);
+      
+      expect(await binToken.balanceOf(await sender.getAddress())).to.equal(getTokens(1*(1+DEF_PAYOUT/100000)));
+      expect(await binToken.balanceOf(binaryOptions.address)).to.equal(0);
+    });
+
+    it('should not collect if looser bet for lower', async () => {
+      await binaryOptions.connect(sender).place(timeStamp, getTokens(1), false);
+      timeTravel(INTERVAL*2);
+      await EthPriceFeed.mock.latestRoundData.returns(0, ETH_PRICE + 100, getLastRound(), getLastRound(), 0);
+      await binaryOptions.executeRound(timeStamp);
+      await binaryOptions.connect(sender).collect();
+
+      expect(await binaryOptions.getOptionsLength()).to.equal(1);
+      expect(await binaryOptions.getPendingOptionsLength(await sender.getAddress())).to.equal(0);
+      expect(await binaryOptions.getCollectedOptionsLength(await sender.getAddress())).to.equal(1);
+      
+      expect(await binToken.balanceOf(await sender.getAddress())).to.equal(0);
+      expect(await binToken.balanceOf(binaryOptions.address)).to.equal(getTokens(1));
     });
   });
 });
--- test/utils/time.ts
+++ test/utils/time.ts
@@ -1,9 +1,20 @@
 import { ethers } from 'hardhat';
 
-const send = (method, params = []) =>
+const send = (method: string, params: any[] = []) =>
   ethers.provider.send(method, params);
 
-export const timeTravel = async seconds => {
-  await send('evm_increaseTime', [seconds])
-  await send('evm_mine')
+export const timeTravel = async (seconds: number) => {
+  await send('evm_increaseTime', [seconds]);
+  await send('evm_mine');
 };
+
+export const timeSet = async (seconds: number) => {
+  await send('evm_setNextBlockTimestamp', [seconds]);
+  await send('evm_mine');
+}
+
+export const timeSave = async () =>
+  await send('evm_snapshot');
+
+export const timeRevert = async (id: number) =>
+  await send('evm_revert', [id]);
